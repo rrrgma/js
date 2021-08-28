85486d3eaa55680528b27488f27da5007b473cdf
Agatha Melissa <agathamgrrr@gmail.com>
2021-08-28 15:43:29 -0300

    tipos primitivos

---
+++ tipos primitivos/index.js
@@ -0,0 +1,18 @@
+/*
+String
+Number
+Boolean
+Null
+Undefined
+*/
+
+let nome = "Ágatha Melissa";
+let idade = 25;
+let boolean = true;
+let undefined;
+
+console.log(typeof nome, nome);
+console.log(typeof idade, idade);
+console.log(typeof boolean, boolean);
+console.log(typeof nullo, nullo);
+console.log(typeof undefined, undefined);
