12c0da70bf672ec58814fb95161ccf75f00747b7
Agatha Melissa <agathamgrrr@gmail.com>
2021-09-02 20:47:38 -0300

    js

---
+++ 06 operadores de comparação/index.js
@@ -0,0 +1,44 @@
+/*
+> maior que
+< menor que
+== igual
+=== igual restrita
+! diferente
+>= maior ou igual
+*/
+
+/*operador maior que
+
+let idade =25;
+
+console.log(idade > 30); */
+
+
+
+/*operador de igualdade
+
+let idade =29;
+
+console.log(idade) ==29);
+*/
+
+
+
+/*operador diferente
+
+let idade = 30;
+
+// console.log(idade !=30);
+
+if (){ // Se essa validação ou comparação for verdadeira
+ console.log("Deu bom");
+} else { // Caso de false cai aqui
+console.log("Deu ruim");
+}
+*/
+
+
+//operador maior ou igual 
+let idade = 29;
+
+console.log(idade >= 49);
